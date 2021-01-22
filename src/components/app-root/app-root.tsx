f0134b35c10d33b146a24cee6452f0a1e6abb25a
Ester Gama <esterfaniasantos@gmail.com>
2021-01-22 10:02:55 -0300

    first commit

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -3,20 +3,17 @@ import { Component, h } from '@stencil/core';
 @Component({
   tag: 'app-root',
   styleUrl: 'app-root.css',
-  shadow: true,
 })
 export class AppRoot {
   render() {
     return (
-      <div>
-        <header>
-          <app-title title="Lista de Tarefas"></app-title>
-        </header>
-
-        <main>
-          <app-todo></app-todo>
-        </main>
-      </div>
+      <ion-app>
+        <ion-router useHash={false}>
+          <ion-route url="/" component="app-home" />
+          <ion-route url="/profile/:name" component="app-profile" />
+        </ion-router>
+        <ion-nav />
+      </ion-app>
     );
   }
 }
